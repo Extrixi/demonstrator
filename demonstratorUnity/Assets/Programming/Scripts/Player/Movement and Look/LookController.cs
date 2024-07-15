37792f2f645519b4aa3cb6a2eef5b8c58967ab5c
DM <dommcneill@outlook.com>
2024-07-15 14:03:56 +0100

    movement

--- demonstratorUnity/Assets/Programming/Scripts/Player/Movement and Look/LookController.cs
+++ demonstratorUnity/Assets/Programming/Scripts/Player/Movement and Look/LookController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class LookController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Transform CameraHolder;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float Sense = 1f;
+
+	private float _yRotation;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 MouseLook = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+		_yRotation -= MouseLook.y * Sense;
+
+		_yRotation = Mathf.Clamp(_yRotation, -90, 90);
+
+		CameraHolder.Rotate(transform.up * MouseLook.x * Sense);
+
+		CameraHolder.rotation = Quaternion.Euler(_yRotation, CameraHolder.rotation.eulerAngles.y, 0);
+	}
 }
