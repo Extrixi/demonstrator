198d180b774e1e749b10ae50582ebfde3b5a3ca9
DM <dommcneill@outlook.com>
2024-07-17 22:54:10 +0100

    Basic player made
    Awaiting approval for movement and requests.
    Implemented player movement and mouse look.
    Touched some extra thing. preying that it was a fix to my crashes.

--- demonstratorUnity/Assets/Programming/Scripts/Player/Movement and Look/MovementController.cs
+++ demonstratorUnity/Assets/Programming/Scripts/Player/Movement and Look/MovementController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
@@ -8,7 +9,22 @@ public class MovementController : MonoBehaviour
 	private Transform _orientation;
 
 	[SerializeField]
-	private float _TargetSpeed = 50;
+	private float _targetSpeed = 30;
+
+	[SerializeField]
+	private float _accellerationRate = 0.5f;
+
+	[SerializeField]
+	private float _deAccellerationRate = 0.5f;
+
+	[SerializeField]
+	private float _turnForce = 10f;
+
+	[SerializeField]
+	private float _minimumStopSpeed = 0.1f;
+
+	[SerializeField]
+	private float _turnSpeed = 0.5f;
 
 	private Rigidbody rb;
 
@@ -28,27 +44,48 @@ public class MovementController : MonoBehaviour
 		if (wishDir.z > 0)
 		{
 
-			Vector3 targ = (_orientation.forward * _TargetSpeed) - rb.velocity;
+			Vector3 targ = ((_orientation.forward * _targetSpeed) - rb.velocity) * _accellerationRate;
 
-			targ *= 0.5f;
-
+			// we dont want to effect the y.
+			targ.y = 0;
 
 			rb.AddForce(targ, ForceMode.Force);
 
-			print(rb.velocity);
+
+
+		}
+		else if (wishDir.z < 0)
+		{
+			Vector3 targ = -rb.velocity * _deAccellerationRate;
+
+			targ.y = 0;
+
+			if (rb.velocity.magnitude < _minimumStopSpeed)
+			{
+				rb.velocity = new Vector3(0, rb.velocity.y, 0);
+			}
+			else
+			{
+				rb.AddForce(targ, ForceMode.Force);
+			}
+
 		}
 
 		// rotate the player
 		if (wishDir.x != 0)
 		{
 
-			_orientation.Rotate(_orientation.up * wishDir.x * 0.5f);
+			_orientation.Rotate(_orientation.up * wishDir.x * _turnSpeed);
+
+			Vector3 targ = (_orientation.right * wishDir.x * _turnForce + _orientation.forward * _turnForce).normalized * rb.velocity.magnitude - rb.velocity;
+
+			targ.y = 0;
+
+
+			rb.AddForce(targ, ForceMode.Force);
 		}
 
-		// wishDir = wishDir.normalized * _TargetSpeed;
-
-		// Vector3 targetVel = wishDir - rb.velocity;
-
+		print(rb.velocity + " | " + rb.velocity.magnitude);
 
 	}
 }
